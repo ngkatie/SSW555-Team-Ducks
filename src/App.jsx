91f341d88723dd821998983570aabe6be06b634f
Kyle <kyleponte07@gmail.com>
2025-03-09 22:45:26 -0400

    Added button to finalize knapsack

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+import React from 'react'
 import { useState } from 'react'
 import qkLogo from '/logo.png'
 import './App.css'
+import FinalizeButton from './components/FinalizeButton/FinalizeButton'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isFinalized, setIsFinalized] = useState(false);
+
+  const handleFinalize = () => {
+    setIsFinalized(true);
+  };
 
   return (
     <>
@@ -15,10 +21,14 @@ function App() {
       <h1>Quantum Knapsack</h1>
       <div className="card">
         <p>
-        To address the learning curve associated with quantum computing, Quantum Knapsack introduces foundational concepts using a 
-        variation of the classic knapsack problem. Through steady progression to higher-level concepts, our mission is to design puzzles 
-        that spark interest in quantum computing without advanced math prerequisites.
+          To address the learning curve associated with quantum computing, Quantum Knapsack introduces foundational concepts using a 
+          variation of the classic knapsack problem. Through steady progression to higher-level concepts, our mission is to design puzzles 
+          that spark interest in quantum computing without advanced math prerequisites.
         </p>
+        <FinalizeButton 
+          isFinalized={isFinalized}
+          onFinalize={handleFinalize}
+        />
       </div>
       <p className="read-the-docs">
         Come back soon! Work in progress...
