ebfc92aad3333bf25f56a9a22aa8cc72251cff1b
Katie Ng <ngkatie16@gmail.com>
2025-03-28 20:40:34 -0400

    Enhanced landing page UI

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -6,18 +6,15 @@ import './LandingPage.css';
 function LandingPage() {
   return (
     <div className="landing-page">
-      <div>
+      {/* <div>
         <img src={qkLogo} className="logo" alt="Quantum Knapsack logo" />
-      </div>
+      </div> */}
       <h1>Quantum Knapsack</h1>
       <div className="card">
         <p>
           To address the learning curve associated with quantum computing, Quantum Knapsack introduces foundational concepts using a variation of the classic knapsack problem.
         </p>
       </div>
-      <p className="read-the-docs">
-        Come back soon! Work in progress...
-      </p>
       <Link to="/game">
         <button>Start Game</button>
       </Link>
