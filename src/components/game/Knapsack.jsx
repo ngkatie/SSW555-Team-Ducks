13923f9890fd0ec5bc42aa3be946debb8df99283
Katie Ng <ngkatie16@gmail.com>
2025-04-01 02:32:46 -0400

    Add decoherence functionality

--- src/components/game/Knapsack.jsx
+++ src/components/game/Knapsack.jsx
@@ -1,38 +1,49 @@
-export default class Knapsack {
-    // Defines knapsack object
-    constructor(capacity) {
-        this.capacity = capacity;
-        this.qubits = [];
-        this.currentWeight = 0;
-        this.currentValue = 0;
-    }
+import React, {useState, useEffect} from "react";
 
-    // Creates knapsack object
-    static createKnapsack = (capacity) => {
-        return new Knapsack(capacity);
-    };
+const Knapsack = ({initialCapacity = 100}) => {
+
+    const [capacity, setCapacity] = useState(initialCapacity);
+    const [weight, setWeight] = useState(0);
+    const [value, setValue] = useState(0);
+    const [qubits, setQubits] = useState([]);
+
+    useEffect(() => {
+        const reduceCapacity = () => {
+            setCapacity(Math.max(capacity * 0.9, 0));
+            nextDecoherence();
+        };
+
+        const nextDecoherence = () => {
+            const interval = Math.random() * 30000;
+            setTimeout(reduceCapacity, interval);
+        };
+
+        nextDecoherence();
+
+        return () => clearTimeout();
+    }, []);
 
     // Updates knapsack after qubit selection and deselection
     updateKnapsack = (qubit, action) => {
         // performs 'selection'
         if (action === 'select') {
             // checks if knapsack capacity is exceeded after adding a qubit
-            if (this.currentWeight + qubit.weight > this.capacity)
+            if (weight + qubit.weight > capacity)
                 throw new Error("Knapsack capacity exceeded!");
 
-            this.qubits.push(qubit);
-            this.currentWeight += qubit.weight;
-            this.currentValue += qubit.value;
+            setQubits(currentQubits => [...currentQubits, qubit]);
+            setWeight(currentWeight => currentWeight + qubit.weight);
+            setValue(currentValue => currentValue + qubit.value);
         }
         // performs 'deselection'
         else if (action === 'deselect') {
              // checks if knapsack capacity is below lower bound after removing a qubit
-            if (this.currentWeight - qubit.weight < 0) 
+            if (weight - qubit.weight < 0) 
                 throw new Error("Knapsack capacity cannot be negative!");
 
-            this.qubits = this.qubits.filter(q => q !== qubit);
-            this.currentWeight -= qubit.weight;
-            this.currentValue -= qubit.value;
+            setQubits(qubits.filter(q => q !== qubit));
+            setWeight(currentWeight => currentWeight - qubit.weight);
+            setValue(currentValue => currentValue - qubit.value);
         }
         else {
             throw new Error("Could not update knapsack!");
@@ -40,4 +51,6 @@ export default class Knapsack {
 
         return this; 
     };
-}
+};
+
+export default Knapsack;
