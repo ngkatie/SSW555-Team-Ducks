89a77f3f8891834c2ca14f6b2271af989e394c6d
Kyle <kyleponte07@gmail.com>
2025-04-03 15:05:58 -0400

    Wrote explanations for quantum concepts

--- src/components/game/GamePage.jsx
+++ src/components/game/GamePage.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Box } from '@mui/material';
 import Welcome from './Welcome/Welcome'
+import Round2 from './Rounds/Round2/Round2'
+import Round3 from './Rounds/Round3/Round3'
 import Knapsack from './Knapsack/Knapsack';
 import Qubit from './Qubit/Qubit';
 
